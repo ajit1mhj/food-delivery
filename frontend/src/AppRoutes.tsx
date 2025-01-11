c73e58bcb46755fa994faf0e784bd4225fd6bac5
ajit maharjan <ajit1mhj@gmail.com>
2025-01-11 22:59:26 +0545

    api and accesstoken

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import  Layout  from "./layouts/layout" ;
 import {Navigate, Route, Routes} from "react-router-dom"
 import HomePage from "./pages/HomePage";
+import AuthCAllbackPage from "./pages/AuthCallbackpage";
 
 const AppRoutes = ()=>{
     return(
         <Routes>
             <Route path="/" element={<Layout><HomePage/></Layout>} />
+            <Route path="/auth-callback" element={<AuthCAllbackPage/>}/>
             <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
             <Route path="#" element={<Navigate to ="/"/>} />
         </Routes>
